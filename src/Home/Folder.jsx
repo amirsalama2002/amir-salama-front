16fee773653fc2812e3dc7ae52b3e83439f85e28
Amir Salama <hassanamirsalama.com>
2025-04-03 12:24:48 +0200

    welcome

--- src/Home/Folder.jsx
+++ src/Home/Folder.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Folder() {
   return (
-    <div>Welcome for loop MIR </div>
+    <div>
+      <h1>Amir Salama Hassan Falcon For loop Start Ahmed Hassan</h1>
+    </div>
   )
 }
