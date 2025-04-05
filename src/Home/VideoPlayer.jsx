165c37f7f6e104b0cab91113b9f698ba97468c07
Amir Salama <hassanamirsalama.com>
2025-04-05 08:16:23 +0200

    hassan salama name

--- src/Home/VideoPlayer.jsx
+++ src/Home/VideoPlayer.jsx
@@ -4,7 +4,8 @@ const VideoPlayer = () => {
     return (
       <div className="vio">
         <div className="fo">
-          <video controls className="">
+        {/* <video playsinline="" autoplay="" controls="" loop="" muted="" src="/imgs/WPP Open_ transforming creative collaboration with AI​.mp4" data-src="/imgs/WPP Open_ transforming creative collaboration with AI​.mp4"></video> */}
+          <video playsinline autoplay controls loop muted>
             <source src="/imgs/WPP Open_ transforming creative collaboration with AI​.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
@@ -14,3 +15,4 @@ const VideoPlayer = () => {
   };
   
   export default VideoPlayer;
+
